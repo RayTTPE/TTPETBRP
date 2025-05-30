73134de46acb9d215412aebb8f660147d7783f90
RayTTPE <kantaphon.masu@bumail.net>
2025-05-31 03:46:52 +0700

    Update contact.py

--- ติดต่อ/contact.py
+++ ติดต่อ/contact.py
@@ -10,11 +10,14 @@ def is_valid_email(email):
     return re.match(email_pattern, email) is not None
 
 def contact_form():
-    with st.form("contact_form"):
-        name = st.text_input("First Name")
-        email = st.text_input("Email Address")
-        message = st.text_area("Your Message")
-        submit_button = st.form_submit_button("Submit")
+    if "form_submitted" not in st.session_state:
+        st.session_state["form_submitted"] = False
+    if not st.session_state["form_submitted"]:
+        with st.form("contact_form"):
+            name = st.text_input("First Name")
+            email = st.text_input("Email Address")
+            message = st.text_area("Your Message")
+            submit_button = st.form_submit_button("Submit")
 
     if submit_button:
         if not WEBHOOK_URL:
